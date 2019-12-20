4093efd4bc66b7a5858c4dc8654beeb424083820
stwinst <s_tsolak1978@yahoo.gr>
2019-12-20 22:20:34 +0200

    dataBinding Commit

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -8,20 +8,24 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var  binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+       // findViewById<Button>(R.id.done_button).setOnClickListener {
+       //     addNickname(it)
+
+        //
+       // }
+        binding.doneButton.setOnClickListener{
             addNickname(it)
-
-            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.hideSoftInputFromWindow(it.windowToken, 0)
         }
 
 
@@ -29,12 +33,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun addNickname(it: View?) {
 
-        val getnickname = findViewById<EditText>(R.id.editText)
-        val mtextview = findViewById<TextView>(R.id.nickname_text)
+        binding.apply{
+            nicknameText.text = binding.editText.text
+            invalidateAll()
+            editText.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+        //Hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(it?.windowToken,0)
 
-        mtextview.setText(getnickname.text)
-        
-        mtextview.setVisibility(TextView.VISIBLE)
-        getnickname.setVisibility(EditText.GONE)
     }
 }
